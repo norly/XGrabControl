12ed2a553cc8b73e91eb2ad0b416ef44d2097b36
norly <norly@enpas.org>
2011-10-10 00:52:44 +0100

    Initial commit.

---
+++ grab-replacement.c
@@ -0,0 +1,33 @@
+/*
+  grab-replacement.c, part of XGrabControl.
+  (c) 2011, en passant development.
+
+  XGrabControl is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  XGrabControl is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with XGrabControl.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <sys/types.h>
+#include <X11/Xlib.h>
+
+
+int XGrabKeyboard(Display *display, Window grab_window, Bool owner_events, int pointer_mode, int keyboard_mode, Time time)
+{
+  (void) display;
+  (void) grab_window;
+  (void) owner_events;
+  (void) pointer_mode;
+  (void) keyboard_mode;
+  (void) time;
+
+  return GrabSuccess;
+}
